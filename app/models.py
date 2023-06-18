4ec0ca51a05a22428242ea3f1e0efc0a8d05ee2b
mar0ve <marveevram@yandex.ru>
2023-06-18 03:36:09 +0300

    добавлена страница профиля, аватары + добавлена возможность редактирования имени и обо мне

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
 from app import db, login
+from hashlib import md5
 
 
 class User(UserMixin, db.Model):
@@ -10,12 +11,19 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
     
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
     
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -34,3 +42,5 @@ class Post(db.Model):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+
